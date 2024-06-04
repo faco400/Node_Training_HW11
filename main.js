75af1f732e88184c9f0f0f79c96d733e08c77003
faco400 <vd.souza1999@gmail.com>
2024-06-04 20:31:30 -0300

    implemented parseArray function

--- main.js
+++ main.js
@@ -25,44 +25,23 @@ function myJSONParse(jsonString) {
     // getting token from tokens and increment index
     const token = tokens[index++];
     // for(let token of tokens) {
-      if(token === null) // if token of value null return null
+      if(token === 'null') // if token of value null return null
         return null;
-      if(token === true) // if token is of value true return true
+      if(token === 'true') // if token is of value true return true
         return true;
-      if(token === false) // if token is of value false return false
+      if(token === 'false') // if token is of value false return false
         return false;
       if(token === '{') // if tokens if start of object then parse object and return it
         return parseObject();
-      // if(token === '[')
-        // return parseArray();
+      if(token === '[')
+        return parseArray();
       if(/^-?[0-9]+(\.[0-9]+)?/.test(token)) // if its number parse into floating point
         return parseFloat(token);
 
       //if token starts a string then processes it to escape correctly
-      if(token[0] === '"') {
-        return token.slice(1, -1).replace(/\\(["\\\/bfnrt]|u[0-9a-fA-F]{4})/g, (match, p1) => {
-          switch(p1[0]) {
-            case '"':
-              return '"';
-            case '\\':
-              return '\\';
-            case '/':
-              return '/';
-            case 'b':
-              return '\b';
-            case 'f':
-              return '\f';
-            case 'n':
-              return '\n';
-            case 'r':
-              return '\r';
-            case 't':
-              return '\t';
-            case 'u':
-                return String.fromCharCode(parseInt(p1.slice(1), 16));
-          }
-        });
-      }
+      if(token[0] === '"')
+        return token.slice(1, -1);
+      
       throw new Error(`Unexpected token ${token}`);
     // }
   }
@@ -81,9 +60,11 @@ function myJSONParse(jsonString) {
     //iterating through object string
     while(true){
       token = tokens[index++];
+      console.log(token);
       //if token start does not have double quotes of string key throw error
       if (token[0] !== '"')
         throw new Error('Expected string as key');
+
       const key = token.slice(1,-1); // getting key of object
 
       // if there is not colon after token throw error
@@ -96,28 +77,65 @@ function myJSONParse(jsonString) {
       //if next token is end of object, break loop
       token = tokens[index++];
       if(token === '}')
-        break;
+        return obj;
 
       // if next token is not comma then throw error
       if(token !== ',')
         throw new Error('Expected comma or closing brace');
 
     }
-    return obj;
+
+  }
+
+  function parseArray() {
+    const array = []; //array to be returned
+
+     // getting token from tokens and increment index
+     let token = tokens[index++];
+
+     //if token === } then is the end of array, return it
+     if(token === ']')
+       return array;
+     
+     index--; //go back to beginning of array
+
+     while(true) {
+       array.push(parseValue());
+       console.log(array);
+
+      //if next token is end of array, break loop
+      token = tokens[index++];
+      if(token === ']')
+        return array;
+
+      // if next token is not comma then throw error
+      if(token !== ',')
+        throw new Error('Expected comma or closing brace');
+     }
   }
 
   return parseValue()
 }
 
-console.log(myJSONParse('{"name": "Vini", "age": 32}'))
 
+// ------------testing object---------------
 // const jsonString = '{"name": "John", "age": 30, "city": "New York"}';
 // const jsonObject = myJSONParse(jsonString);
 
 // console.log(jsonObject); // Should output the parsed JavaScript object.
-  
-// console.log(tokenize('{"name": "Vini"}'));
-// const json = '{"result":true, "count":42}';
-// const obj = JSON.parse(json);
+// -----------------------------------------
+// ------------testing object with boolean values---------------
+// const jsonString = '{"name":"John", "age":30, "car":null , "brazilian": false, "american" : true}';
+// const jsonObject = myJSONParse(jsonString);
 
-// console.log(obj);
+// console.log(jsonObject); // Should output the parsed JavaScript object.
+// ------------testing object with array---------------
+// const jsonString = '{"name":"John", "age":30, "daughters": ["Jane", "Monica"]}';
+// const jsonObject = myJSONParse(jsonString);
+
+// console.log(jsonObject); // Should output the parsed JavaScript object.
+// ------------testing object with intern object---------------
+// const jsonString = '{"name":"John", "age":30, "daughters": {"name": "Jane", "age": 10}}';
+// const jsonObject = myJSONParse(jsonString);
+
+// console.log(jsonObject); // Should output the parsed JavaScript object.
